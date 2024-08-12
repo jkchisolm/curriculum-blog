a910b3c34e5b6852408fed7058af4aebc07934b1
Joshua Chisolm <jkchisolm22@gmail.com>
2024-08-12 15:18:39 -0700

    pull posts from DB on homepage

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -12,16 +12,19 @@ const Card = ({ key, item }) => {
       )}
       <div className={styles.textContainer}>
         <div className={styles.detail}>
-          <span className={styles.date}>
+          {/* <span className={styles.date}>
             {item.createdAt.substring(0, 10)} -{" "}
-          </span>
+          </span> */}
           <span className={styles.category}>{item.catSlug}</span>
         </div>
         <Link href={`/posts/${item.slug}`}>
           <h1>{item.title}</h1>
         </Link>
         {/* <p className={styles.desc}>{item.desc.substring(0, 60)}</p> */}
-        <div className={styles.desc} dangerouslySetInnerHTML={{ __html: item?.desc.substring(0,60) }}/>
+        <div
+          className={styles.desc}
+          dangerouslySetInnerHTML={{ __html: item?.desc.substring(0, 60) }}
+        />
         <Link href={`/posts/${item.slug}`} className={styles.link}>
           Read More
         </Link>
--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -3,37 +3,44 @@ import styles from "./cardList.module.css";
 import Pagination from "../pagination/Pagination";
 import Image from "next/image";
 import Card from "../card/Card";
+import { collection, getDocs } from "firebase/firestore";
+import db from "@/utils/firestore";
 
 const getData = async (page, cat) => {
-  const res = await fetch(
-    `http://localhost:3000/api/posts?page=${page}&cat=${cat || ""}`,
-    {
-      cache: "no-store",
-    }
-  );
+  const postsCol = collection(db, "posts");
+  const postsSnapshot = await getDocs(postsCol);
+  const postsList = postsSnapshot.docs.map((doc) => doc.data());
 
-  if (!res.ok) {
-    throw new Error("Failed");
-  }
+  return { posts: postsList, count: postsList.length };
+  // const res = await fetch(
+  //   `http://localhost:3000/api/posts?page=${page}&cat=${cat || ""}`,
+  //   {
+  //     cache: "no-store",
+  //   }
+  // );
 
-  return res.json();
+  // if (!res.ok) {
+  //   throw new Error("Failed");
+  // }
+
+  // return res.json();
 };
 
 const CardList = async ({ page, cat }) => {
-  // const { posts, count } = await getData(page, cat);
+  const { posts, count } = await getData(page, cat);
 
-  // const POST_PER_PAGE = 2;
+  const POST_PER_PAGE = 2;
 
-  // const hasPrev = POST_PER_PAGE * (page - 1) > 0;
-  // const hasNext = POST_PER_PAGE * (page - 1) + POST_PER_PAGE < count;
+  const hasPrev = POST_PER_PAGE * (page - 1) > 0;
+  const hasNext = POST_PER_PAGE * (page - 1) + POST_PER_PAGE < count;
 
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Recent Posts</h1>
       <div className={styles.posts}>
-        {/* {posts?.map((item) => (
+        {posts?.map((item) => (
           <Card item={item} key={item._id} />
-        ))} */}
+        ))}
       </div>
       {/* <Pagination page={page} hasPrev={hasPrev} hasNext={hasNext} /> */}
     </div>

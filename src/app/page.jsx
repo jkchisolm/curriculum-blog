7bc3c3d649b40a3478895cb82baa7fa2491b9513
Joshua Chisolm <jkchisolm22@gmail.com>
2024-08-12 23:31:40 -0700

    remove menu as we won't use that

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,5 @@
-import Link from "next/link";
 import styles from "./homepage.module.css";
-import Featured from "@/components/featured/Featured";
-import CategoryList from "@/components/categoryList/CategoryList";
 import CardList from "@/components/cardList/CardList";
-import Menu from "@/components/Menu/Menu";
 
 export default function Home({ searchParams }) {
   const page = parseInt(searchParams.page) || 1;
@@ -12,7 +8,6 @@ export default function Home({ searchParams }) {
     <div className={styles.container}>
       <div className={styles.content}>
         <CardList page={page} />
-        {/* <Menu /> */}
       </div>
     </div>
   );

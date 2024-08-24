ac16c80477f9be6130bfdfb158e93a4c0e49d7e6
Joshua Chisolm <jkchisolm22@gmail.com>
2024-08-23 22:38:28 -0700

    rm unused part of an API route

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -4,21 +4,6 @@ import { collection, getDocs, addDoc } from "firebase/firestore";
 import db from "@/utils/firestore";
 
 export const GET = async (req) => {
-  const { searchParams } = new URL(req.url);
-
-  const page = searchParams.get("page");
-  const cat = searchParams.get("cat");
-
-  const POST_PER_PAGE = 2;
-
-  const query = {
-    take: POST_PER_PAGE,
-    skip: POST_PER_PAGE * (page - 1),
-    where: {
-      ...(cat && { catSlug: cat }),
-    },
-  };
-
   try {
     const posts = await getPosts();
     const count = posts.length;
